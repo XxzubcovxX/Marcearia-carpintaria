49bb03b59b32d329b80a82ba14bcd1c4cc39ef0f
André Zubcov <andre02zub@gmail.com>
2025-08-01 15:40:53 -0300

    header e footer

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+using System.Diagnostics;
+using Marcenaria_Ismael.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Marcenaria_Ismael.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Servicos()
+        {
+            return View();
+        }
+        
+        public IActionResult Contatos()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Marcenaria_Ismael.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
